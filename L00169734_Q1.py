ad4159d8cb6329d10c08c349a6d75a2f74273f02
AngieBrennan <91499266+AngieBrennan@users.noreply.github.com>
2021-11-08 21:40:56 +0000

    Inserting Description

---
+++ L00169734_Q1.py
@@ -0,0 +1,25 @@
+"""
+#
+# File        : L00169734_Q1.py
+# Created     ：08/11/2021 18:37
+# Author      ：Angela Brennan
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Angela Brennan, LYIT
+#            Available under GNU Public License (GPL)
+# Description ：Q1. Connect to virtual machine using ssh port
+#
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application
+
+      Using paramiko to connect to VM
+
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
+    #
